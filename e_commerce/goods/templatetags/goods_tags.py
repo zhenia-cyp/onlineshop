2a8d083621e87e2bd7d2953c6a9830b960f39295
zhenia <zhenia@ubuntu20.04>
2024-04-03 15:52:03 +0300

    Attached the call of the template tag change_params to the previous and next buttons

--- e_commerce/goods/templatetags/goods_tags.py
+++ e_commerce/goods/templatetags/goods_tags.py
@@ -7,6 +7,7 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def change_params(context, **kwargs):
+    """returns a dictionary of GET request parameters from the request object"""
     query = context['request'].GET.dict()
     query.update(kwargs)
     return urlencode(query)
