45dddbee968162fb74fe33b0d55c53569712e1b4
zhenia-cyp <kviat.zhenia@gmail.com>
2024-07-12 17:34:42 +0300

    Made the order creation function and configured order display in the admin panel

--- e_commerce/users/admin.py
+++ e_commerce/users/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from users.models import User
 from carts.admin import CartTabAdmin
+from orders.admin import OrderTabulareAdmin
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "first_name", "last_name", "email",]
     search_fields = ["username", "first_name", "last_name", "email",]
 
-    inlines = [CartTabAdmin, ]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
